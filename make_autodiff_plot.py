5ec76f419f609ef8d2e8027df8dd46e16e59584a
Oliver Hahn <oliverjhahn@gmail.com>
2023-07-26 07:54:11 +0200

    more plot cleanup

--- make_autodiff_plot.py
+++ make_autodiff_plot.py
@@ -1,14 +1,9 @@
 import jax
-import numpy as np
 import jax.numpy as jnp
-import equinox as eqx
-import diffrax
 import matplotlib.pyplot as plt
 
-jax.config.update("jax_enable_x64", True)
-# jax.config.update('jax_platform_name', 'cpu')
-# jax.config.update("jax_debug_nans", True)
-# jax.config.update("jax_disable_jit", True)
+# unfortunately autodiff currently needs double precision!
+jax.config.update("jax_enable_x64", True) 
 
 from pylinger_background import evolve_background
 from pylinger_perturbations import evolve_one_mode, evolve_perturbations
@@ -24,7 +19,7 @@ w_DE_0  = -0.99
 w_DE_a  = 0.0
 cs2_DE  = 0.99
 num_massive_neutrinos = 1
-mnu     = 0.06 #0.06 #eV
+mnu     = 0.06  #eV
 Neff    = 2.046 # -1 if massive neutrino present
 standard_neutrino_neff=Neff+num_massive_neutrinos
 H0      = 67.742
@@ -32,12 +27,13 @@ A_s     = 2.1064e-09
 n_s     = 0.96822
 
 
+# list of parameters with respect to which we take derivatives
 fieldnames = ['\\Omega_m', '\\Omega_b', 'A_s', 'n_s', 'H_0', 'T_{CMB}', 'Y_{He}', 'N_{eff}', 'm_{\\nu}', 'w_0', 'w_a', 'c_s^2']
 fiducial_cosmo_param = jnp.array([Omegam, Omegab, A_s, n_s, H0, Tcmb, YHe, Neff, mnu, w_DE_0, w_DE_a, cs2_DE])
 
 
-
-def f_of_Omegam( args ):
+def Pk_of_cosmo( args ):
+    """ Compute the matter (b+c) power spectrum for a given set of cosmological parameters"""
     param = {}
     param['Omegam'] = args[0]
     param['Omegab'] = args[1]
@@ -57,9 +53,9 @@ def f_of_Omegam( args ):
 
     k_p  = 0.05
 
+    ## compute the background evolution
     param = evolve_background(param=param)
     
-    k = 1e-2
 
     # Compute Perturbations
     lmaxg  = 12
@@ -69,16 +65,17 @@ def f_of_Omegam( args ):
     nqmax  = 15
 
     rtol   = 1e-3
-    atol   = 1e-4
+    atol   = 1e-5
 
     # Compute Perturbations
     nmodes = 256
     kmin = 1e-4
     kmax = 1e1
-    # aexp_out = jnp.array([1e-2,1e-1]) 
     aexp_out = jnp.geomspace(1e-2,1,2)
 
-    y, kmodes = evolve_perturbations( param=param, kmin=kmin, kmax=kmax, num_k=nmodes, aexp_out=aexp_out )
+    y, kmodes = evolve_perturbations( param=param, kmin=kmin, kmax=kmax, num_k=nmodes, aexp_out=aexp_out,
+                                      lmaxg=lmaxg, lmaxgp=lmaxgp, lmaxr=lmaxr, lmaxnu=lmaxnu, nqmax=nqmax,
+                                       rtol=rtol, atol=atol )
 
     iout = -1
     fac = 2.5
@@ -89,14 +86,12 @@ def f_of_Omegam( args ):
     return Pkm
 
 
-#print( f_of_Omegam(fiducial_cosmo_param).shape )
-
-## compute the jacobian at few times, but for many k
+## compute the jacobian 
 k  = jnp.geomspace(1e-4,1e1,256)
-dy = jax.jacfwd(f_of_Omegam)(fiducial_cosmo_param)
+dy = jax.jacfwd(Pk_of_cosmo)(fiducial_cosmo_param)
 
-#print( dy.shape )
 
+## make the plot
 fig,ax = plt.subplots(4,3,sharex=True,figsize=(13,10),layout='constrained')
 
 for i,ff in enumerate(fieldnames):
