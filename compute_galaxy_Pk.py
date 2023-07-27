2e2f216a211084ee8923b9e9e69e5a92a53aad3c
Oliver Hahn <oliverjhahn@gmail.com>
2023-07-27 16:42:29 +0200

    added code to compute a (linearly) biased galaxy spectrum

---
+++ compute_galaxy_Pk.py
@@ -0,0 +1,128 @@
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+
+# unfortunately autodiff currently needs double precision!
+jax.config.update("jax_enable_x64", True) 
+
+from pylinger_background import evolve_background
+from pylinger_perturbations import evolve_one_mode, evolve_perturbations
+
+
+## Cosmological Parameters
+Tcmb    = 2.7255
+YHe     = 0.248
+Omegam  = 0.3099
+Omegab  = 0.0488911
+# OmegaDE = 1.0-Omegam
+w_DE_0  = -0.99
+w_DE_a  = 0.0
+cs2_DE  = 0.99
+num_massive_neutrinos = 1
+mnu     = 0.06  #eV
+Neff    = 2.046 # -1 if massive neutrino present
+standard_neutrino_neff=Neff+num_massive_neutrinos
+H0      = 67.742
+A_s     = 2.1064e-09
+n_s     = 0.96822
+
+## Galaxy Parameters
+b1      = 1.3  # linear bias, should be close to 1.3 for ELGs
+
+# list of parameters with respect to which we take derivatives
+fieldnames = ['b_1','\\Omega_m', '\\Omega_b', 'A_s', 'n_s', 'H_0', 'N_{eff}', 'm_{\\nu}', 'w_0', 'w_a']
+fiducial_cosmo_param = jnp.array([b1, Omegam, Omegab, A_s, n_s, H0, Neff, mnu, w_DE_0, w_DE_a])
+
+
+def Pkbiased( args ):
+    """ Compute the matter (b+c) power spectrum for a given set of cosmological parameters"""
+    param = {}
+    a = args[0]
+    b1 = args[1]
+    param['Omegam'] = args[2]
+    param['Omegab'] = args[3]
+    param['OmegaDE'] = 1-args[2]
+    param['Omegak'] = 0.0
+    A_s = args[4]
+    n_s = args[5]
+    param['H0'] = args[6]
+    param['Tcmb'] = Tcmb
+    param['YHe'] = YHe
+    param['Neff'] = args[7]
+    param['Nmnu'] = num_massive_neutrinos
+    param['mnu'] = args[8]
+    param['w_DE_0'] = args[9]
+    param['w_DE_a'] = args[10]
+    param['cs2_DE'] = cs2_DE
+
+    k_p  = 0.05
+
+    ## compute the background evolution
+    param = evolve_background(param=param)
+    
+
+    # Compute Perturbations
+    lmaxg  = 12
+    lmaxgp = 12
+    lmaxr  = 17
+    lmaxnu = 17
+    nqmax  = 15
+
+    rtol   = 1e-3
+    atol   = 1e-6
+
+    # Compute Perturbations
+    nmodes = 256  # number of modes to compute, reduce to speed up calculation
+    kmin = 1e-3
+    kmax = 1e0
+    aexp_out = jnp.geomspace(1e-2,1,2)
+
+    y, kmodes = evolve_perturbations( param=param, kmin=kmin, kmax=kmax, num_k=nmodes, aexp_out=aexp_out,
+                                      lmaxg=lmaxg, lmaxgp=lmaxgp, lmaxr=lmaxr, lmaxnu=lmaxnu, nqmax=nqmax,
+                                       rtol=rtol, atol=atol )
+
+    iout = -1
+    fac = 2.5
+
+    iq0 = 10 + lmaxg + lmaxgp + lmaxr
+
+    Omeganu = (param['grhor'] * rhonu / a**4) / (param['grhom'] / a**3)
+
+    rhonu = param['rhonu_of_a_spline'].evaluate(a)
+    Pkc =  fac * A_s*(kmodes/k_p)**(n_s - 1) * kmodes**(-3) * y[:,iout,3]**2 
+    Pkb =  fac * A_s*(kmodes/k_p)**(n_s - 1) * kmodes**(-3) * y[:,iout,5]**2 
+    Pknu = fac * A_s*(kmodes/k_p)**(n_s - 1) * kmodes**(-3) * y[:,iout,iq0]**2
+    Pkm = (param['Omegam']-param['Omegab']) * Pkc + param['Omegab'] * Pkb + Omeganu * Pknu
+    
+    return b1**2 * Pkm
+
+
+## compute the jacobian 
+k  = jnp.geomspace(1e-3,1e0,256) # number of modes to compute, reduce to speed up calculation
+
+Pk = Pkbiased( fiducial_cosmo_param )
+
+# dy = jax.jacfwd(Pkbiased)(fiducial_cosmo_param)
+
+fig, ax = plt.subplots()
+
+ax.loglog(k, Pk)
+ax.set_xlabel('$k / h Mpc^{-1}$')
+ax.set_ylabel('$P(k)$')
+
+plt.savefig('Pk.pdf')
+
+# ## make the plot
+# fig,ax = plt.subplots(4,3,sharex=True,figsize=(13,10),layout='constrained')
+
+# for i,ff in enumerate(fieldnames):
+#     iy = i//3
+#     ix = i%3
+#     ax[iy,ix].semilogx(k, dy[:,i],label='$P_{b+c}$')
+#     ax[iy,ix].axhline(0.0, ls=':', color='k')
+#     ax[iy,ix].set_title(f'$dP(k) / d{ff}$')
+    
+# for a in ax[-1,:]:
+#     a.set_xlabel('$k / h Mpc^{-1}$')
+
+# plt.savefig('derivatives.pdf')
